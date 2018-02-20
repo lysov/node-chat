3c0e1f72b0b1360c73c00ebbabaefaa774d98a11
Anton Lysov <anton@antonlysov.com>
2018-02-19 19:35:56 -0700

    Code clean up

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
+// Dependencies
+
 // Express
-let app = require('express')();
+const app = require('express')();
 
 // Socket.io
-let server = require('http').Server(app);
-let io = require('socket.io')(server);
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
-// Other Dependencies
-let path = require('path');
-let fs = require('fs');
-
-let cookie = require('cookie');
-
+// Other
+const path = require('path');
+const fs = require('fs');
 const hex_rgb = require('hex-rgb');
 
-let port = 8080;
+// Constants
+const port = 8080;
 
+// Data Model
 let users_online = [];
 let current_unique_id = 0;
-
 let messages = [];
 
 server.listen(port, function() {
@@ -30,51 +30,58 @@ app.get('/', function(req, res) {
 
 io.on('connection', function(socket) {
 
-  console.log("A new user has connected.");
+  console.log('A new user has connected.');
 
-  let cookies = cookie.parse(socket.handshake.headers['cookie']);
-  console.log(cookies.user_id);
-  cookies.user_id = 97;
-  console.log(cookies.user_id);
-
+  // generates a nickname for the new user
   random_nickname(function(nickname, error) {
+
     if (error) {
       console.log('Error');
     } else {
-      console.log("The new user has been given '" + nickname + "' nickname.");
-      let logged_out_user_id = current_unique_id;
-      let nickname_color = {"alpha": 255, "blue": 0, "green": 0, "red": 0};
-      users_online.push({"user_id": current_unique_id, "nickname": nickname, "nickname_color": nickname_color});
+
+      console.log('The new user has been given "' + nickname + '" nickname.');
+
+      // adds the new user to the online users
+      const logged_out_user_id = current_unique_id;
+      const nickname_color = {'alpha': 255, 'blue': 0, 'green': 0, 'red': 0};
+      users_online.push({'user_id': current_unique_id, 'nickname': nickname, 'nickname_color': nickname_color});
+
+      // sends a generated nickname and message history to the new user
+      socket.emit('nickname_and_id_and_message_history', nickname, current_unique_id, messages);
       current_unique_id++;
-      socket.emit('nickname_and_message_history', nickname, messages);
+
+      // sends an updated list of online users to the online users
       io.sockets.emit('users_online_did_change', users_online);
 
       socket.on('disconnect', function() {
 
-        // removes the disconnected user from online users
-        console.log(users_online);
-        users_online = find_and_remove(users_online, 'id', logged_out_user_id)
-        console.log(users_online);
+        // removes the disconnected user from the list of online users
+        users_online.removeIf( function(user_online, idx) {
+          return user_online.user_id === logged_out_user_id;
+        });
 
-        console.log("A user with '" + logged_out_user_id + "' id has disconnected.");
+        console.log('A user with "' + logged_out_user_id + '" id has disconnected.');
 
+        // sends an updated list of online users to the online users
         io.sockets.emit('users_online_did_change', users_online);
       });
     }
   });
 
+  // new message from the client
   socket.on('new_message_from_client', function (message) {
 
-    let command = message.text.substring(0, 11);
+    const command = message.text.substring(0, 11);
 
-    if (command !== undefined && command === "/nickcolor ") {
+    if (command !== undefined && command === '/nickcolor ') {
 
-      let new_nickname_color = message.text.substring(11, 17);
+      const new_nickname_color = message.text.substring(11, 17);
 
       try {
-        let new_nickname_color_rgb = hex_rgb(new_nickname_color);
+        const new_nickname_color_rgb = hex_rgb(new_nickname_color);
 
-        // TODO: change to users
+        // TODO: replace the code below to users
+
         // changes a nickname_color in users_online
         users_online.forEach(function(user_online) {
           if (message.author_nickname === user_online.nickname) {
@@ -84,20 +91,18 @@ io.on('connection', function(socket) {
 
         socket.emit('nickname_color_did_change', new_nickname_color_rgb);
       } catch (error) {
-        console.log("Invalid /nickcolor command");
+        console.log('Invalid /nickcolor command');
       }
     } else {
 
-      let command = message.text.substring(0, 6);
+      const command = message.text.substring(0, 6);
 
-      if (command !== undefined && command === "/nick ") {
+      if (command !== undefined && command === '/nick ') {
 
-        let old_nickname = message.author_nickname;
-        let new_nickname = message.text.substring(6);
+        const old_nickname = message.author_nickname;
+        const new_nickname = message.text.substring(6);
 
-        console.log("users_online");
-        console.log(users_online);
-        // TODO: replace this to users
+        // TODO: replace the code to users
         users_online.forEach(function(user_online) {
 
           // checks if the new nickname is unique
@@ -107,37 +112,29 @@ io.on('connection', function(socket) {
 
           // updates a nickname
           if (user_online.nickname === old_nickname) {
-            console.log('sup');
             user_online.nickname = new_nickname;
             return user_online;
           }
         });
 
-        console.log(users_online);
-
         socket.emit('nickname_did_change', new_nickname);
         io.sockets.emit('users_online_did_change', users_online);
-
 
       } else {
 
         // simple message
 
+        // TODO: replace this to users
+
         users_online.forEach(function(user_online) {
           if (message.author_nickname === user_online.nickname) {
 
-            console.log(message);
-
             message.author_nickname_color = user_online.nickname_color;
-
-            console.log(message);
-
             message.timestamp = Date.now();
             messages.push(message);
-
             console.log("New massage has been recieved: '" + message.text + "'.");
 
-            // TODO: broadcast to all the users
+            // broadcasts to all the users
             io.sockets.emit('new_message_from_server', message);
           }
         });
@@ -158,9 +155,7 @@ function random_nickname(callback) {
   fs.readFile(file_path, 'utf8', function (error, data) {
 
     if (error) {
-
       callback(null, error)
-
     }
 
     let jsonContent = JSON.parse(data);
@@ -192,11 +187,11 @@ function random_nickname(callback) {
 */
 String.prototype.capitalized = function() {
 
-  let array = this.split(" ");
+  let array = this.split(' ');
 
   array = array.map(word => word.charAt(0).toUpperCase() + word.slice(1));
 
-  return array.join(" ");
+  return array.join(' ');
 
 }
 
@@ -209,13 +204,14 @@ function Message(author, text, timestamp) {
 /*
   Removes elements, which properties are equal to the value, from the array.
 */
-function find_and_remove(array, property, value) {
-  array.forEach(function(result, index) {
-    if(result[property] === value) {
-      //Remove from array
-      array.splice(index, 1);
+Array.prototype.removeIf = function(callback) {
+    var i = 0;
+    while (i < this.length) {
+        if (callback(this[i])) {
+            this.splice(i, 1);
+        }
+        else {
+            ++i;
+        }
     }
-  });
-
-  return array;
-}
+};
